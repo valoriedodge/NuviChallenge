87d86cdaa061e6fcf80dd39f0b1de40c85517fb0
valoriedodge <valoriejones@gmail.com>
2016-12-20 10:24:30 -0800

    fixed url path

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 #Nuvi Coding Challenge
-##Download multiple zip files, extract out the xml files, and publish the content of each xml file to a redis list called “NEWS_XML”
 
+Download multiple zip files, extract out the xml files, and publish the content of each xml file to a redis list called “NEWS_XML”
+
+###Installation
+
+You must have redis installed to run this app. You can download redis [here](https://redis.io/download).
+
+You can fork or clone the repository and then run `bundle install`
+
+To download and extract zip files from a different site change `base_url` in `main.rb` to be destination url.
+
+## Explanation
 I used the gems `nokogiri` and `rubyzip` to help with downloading and processing the zip and xml files. I also used the `redis ruby client` to simplify creating and saving data to a redis data structure.
+
+By Valorie Dodge
--- main.rb
+++ main.rb
@@ -4,7 +4,6 @@ require 'zip'
 require 'nokogiri'
 require 'redis'
 require 'fileutils'
-require 'net/http'
 
 redis = Redis.new
 #Access all zip files extensions from URL
@@ -20,68 +19,14 @@ links.each do |m|
   end
 end
 
-##### Try One ########
-# input = open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1481953941683.zip").body
-# Zip::InputStream.open(StringIO.new(input)) do |io|
-#   while entry = io.get_next_entry
-#     puts entry.name
-#     parse_zip_content io.read
-#   end
-# end
-
-##### Try Two ########
-# url = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1481953941683.zip"
-# uri = URI.parse(url)
-# req = Net::HTTP::Get.new(uri.path)
-# filename = './test.zip'
-#
-# # download the zip
-# File.open(filename,"wb") do |file|
-#   Net::HTTP.start(uri.host, uri.port) do |http|
-#     http.get(uri.path) do |str|
-#       file.write str
-#     end
-#   end
-# end
-
-##### Try Three ########
-# uri = URI('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1481953941683.zip')
-# Net::HTTP.start(uri.host, uri.port) do |http|
-#   request = Net::HTTP::Get.new uri
-#   response = http.request request # Net::HTTPResponse object
-#   p response
-# end
-#
-#   http.request request do |response|
-#     open '1481953941683.zip', 'w' do |io|
-#       response.read_body do |chunk|
-#         io.write chunk
-#       end
-#     end
-#   end
-# end
-# url = 'http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1481953941683.zip'
-# zipfilename = open(url)
-
-##### Try To Read new ZipFile ########
-# Zip::ZipFile.open(zipfilename) do |zip|
-#   # zip.each { |entry| p entry.get_input_stream.read } # show contents
-#   zip.each { |entry| p entry.name } # show the name of the files inside
-# end
-
-
 #Create temporary folder for xml files
 Dir.mkdir "xml_files" unless File.exists?("xml_files")
 
-##### Try Four - Where I would really like to use it ########
-# zip_files.each do |zip_file|
-#   url = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/" + zip_file
-#   zipfilename = open(url)
-  # Zip::File.open(zipfilename.path) do |xml_file|
-
-
-  ##### Here I have just downloaded one of the zip files myself to implement the rest of the code ########
-  Zip::File.open("#{Dir.pwd}/1481966378542.zip") do |xml_file|
+#Open each zip file extension and extract the xml files to save to redis
+zip_files.each do |zip_file|
+  url = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/" + zip_file
+  zipfilename = open(url)
+  Zip::File.open(zipfilename.path) do |xml_file|
     # Handle entries one by one
     xml_file.each do |entry|
       name = entry.name
@@ -98,7 +43,7 @@ Dir.mkdir "xml_files" unless File.exists?("xml_files")
       end
     end
   end
-# end
+end
 
 #Delete temporary folder for xml files
 FileUtils.rm_rf('xml_files')
